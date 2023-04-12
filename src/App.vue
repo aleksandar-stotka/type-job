16b4ff564c47c60b15c1b4725d378f8790a3f6ff
premierDrums <premierDrums>
2023-04-11 23:59:40 -1100

    set-jobList

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
   <div class="app">
-    hello, hyrule
+   <JobsList :jobs="jobs"/>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-
+import { defineComponent,reactive, ref, toRefs} from 'vue';
+import job from "./types/job"
+import JobsList from './components/JobsList.vue';
 export default defineComponent({
   name: 'App',
-  components: {}
+  components: {JobsList},
+  setup() {
+    
+   const jobs = ref<job[]>([
+    {title:'farm worker',location:'san francisko',salary:333,id:"1"},
+    {title:'drumer',location:'d hisar',salary:223,id:"2"},
+    {title:'pro bike',location:'Bitola',salary:1000,id:"3"},
+    {title:'eri worker',location:'Struga',salary:333,id:"4"},
+    {title:'guitarist',location:'Belgrad',salary:444,id:"5"}
+   ])
+     return {jobs}
+  },
+ 
+ 
 });
 </script>
 
